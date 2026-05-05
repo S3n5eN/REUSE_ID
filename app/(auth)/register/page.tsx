57e21b60ca5def156e58e3129542138f56be0f08
S3n5eN <bbuku974@gmail.com>
2026-05-05 22:26:49 +0700

    perbaikan halaman register

--- app/(auth)/register/page.tsx
+++ app/(auth)/register/page.tsx
@@ -4,27 +4,32 @@ import { useState } from "react";
 import Image from "next/image";
 import { FaEye, FaEyeSlash } from "react-icons/fa";
 import SuccessPopup from "@/components/SuccessPopup";
+import ErrorPopup from "@/components/ErrorPopup";
+import { useRouter } from "next/navigation";
+import Logo from "@/public/Logo/Logo.svg";
 
 export default function RegisterPage() {
   const [username, setUsername] = useState("");
   const [nameError, setNameError] = useState("");
   const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
   const [emailError, setEmailError] = useState("");
+  const [password, setPassword] = useState("");
   const [passwordError, setPasswordError] = useState("");
   const [showPassword, setShowPassword] = useState(false);
   const [successMessage, setSuccessMessage] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+  const router = useRouter();
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
 
     // Validasi nama
-    if (!username.trim() || username.length < 4) {
-      setNameError("Nama harus minimal 4 karakter dan tidak boleh kosong");
-      return;
+    if (!username.trim()) {
+      setNameError("Nama tidak boleh kosong");
     } else if (!/^[A-Za-z\s]+$/.test(username)) {
       setNameError("Nama hanya boleh huruf alfabet (A-Z)");
-      return;
+    } else if (username.length < 4) {
+      setNameError("Nama minimal terdiri dari 4 karakter");
     } else {
       setNameError("");
     }
@@ -60,8 +65,11 @@ export default function RegisterPage() {
 
       if (res.ok) {
         setSuccessMessage("REGISTER BERHASIL!");
+        setTimeout(() => {
+          router.push("/login");
+        }, 2000);
       } else {
-        alert(data.message);
+        setErrorMessage(data.message);
       }
     } catch (err) {
       console.error("Fetch error:", err);
@@ -70,83 +78,128 @@ export default function RegisterPage() {
   };
 
   return (
-    <div className="flex min-h-screen items-center justify-center bg-white relative">
-      {/* Form Register */}
-      <form
-        onSubmit={handleSubmit}
-        className="bg-white p-8 rounded max-w-4xl flex flex-col gap-4"
-      >
-        <h1 className="text-4xl font-bold text-center text-[#007582]">ReuseID</h1>
-        <h2 className="text-xl font-bold text-center text-black">Buat Akun</h2>
-        <p className="text-sm text-center text-black">
-          Masukkan informasi kamu untuk membuat akun
-        </p>
+    <div className="flex h-screen items-center justify-center bg-white relative">
+      {/*Container kiri: form */}
+      <div className="hidden md:flex flex-1 relative h-screen">
+        <div className="flex-1 flex items-center justify-center px-0">
+          <div className="w-full max-w-sm">
+             {/* Logo ReuseID */}
+            <div className="flex items-center justify-center h-full">
+              <Image src={Logo} alt="Logo ReuseID" width={150} />
+            </div>
+            {/* Form Register */}
+            <div className="mb-0.5 text-center">
+              {/*<h2 className="text-4xl font-bold text-center text-[#007582]">ReuseID</h2>*/}
+              <h2 className="text-xl font-bold text-center text-black">Buat Akun</h2>
+              <p className="text-sm text-center text-black">
+                Masukkan informasi kamu untuk membuat akun
+              </p>
+            </div>
 
-        {/* Input Name */}
-        <div className="flex flex-col gap-0">
-          <label className="text-black text-medium">Name:</label>
-          <input
-            type="text"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-            className="bg-gray-300 text-black p-2 rounded outline-none"
-            required
-          />
-          {nameError && <p className="text-red-500 text-sm">{nameError}</p>}
-        </div>
-
-        {/* Input Email */}
-        <div className="flex flex-col gap-0">
-          <label className="text-black text-medium">Email:</label>
-          <input
-            type="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            className="bg-gray-300 text-black p-2 rounded outline-none"
-            required
-          />
-          {emailError && <p className="text-red-500 text-sm">{emailError}</p>}
-        </div>
-
-        {/* Input Password */}
-        <div className="flex flex-col gap-2">
-          <label className="text-black text-medium">Password:</label>
-          <div className="relative">
-            <input
-              type={showPassword ? "text" : "password"}
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              className="bg-gray-300 text-black p-2 rounded outline-none w-full"
-              required
-            />
-            <button
-              type="button"
-              onClick={() => setShowPassword(!showPassword)}
-              className="absolute right-3 top-2 text-gray-600 hover:text-black cursor-pointer"
+            <form
+              onSubmit={handleSubmit}
+              className="bg-white p-8 rounded max-w-4xl flex flex-col gap-4"
             >
-              {showPassword ? <FaEyeSlash /> : <FaEye />}
-            </button>
+
+              {/* Input Name */}
+              <div className="flex flex-col gap-0">
+                <label className="text-black text-medium p-2">Name:</label>
+                <input
+                  type="text"
+                  value={username}
+                  onChange={(e) => setUsername(e.target.value)}
+                  className="bg-gray-300 text-black p-2 rounded outline-none focus:ring-2 focus:ring-[#007582] focus:shadow-lg transition"
+                  required
+                />
+                {nameError && <p className="text-red-500 text-sm max-w-sm">{nameError}</p>}
+              </div>
+
+              {/* Input Email */}
+              <div className="flex flex-col gap-0">
+                <label className="text-black text-medium p-2">Email:</label>
+                <input
+                  type="email"
+                  value={email}
+                  onChange={(e) => {
+                    setEmail(e.target.value);
+                    if (!e.target.value.endsWith("@gmail.com")) {
+                      setEmailError("Email harus mengandung @gmail.com");
+                    } else {
+                      setEmailError("");
+                    }
+                  }
+                  }
+                  className="bg-gray-300 text-black p-2 rounded outline-none focus:ring-2 focus:ring-[#007582] focus:shadow-lg transition"
+                  required
+                />
+                {emailError && <p className="text-red-500 text-sm max-w-sm">{emailError}</p>}
+              </div>
+
+              {/* Input Password */}
+              <div className="flex flex-col gap-0">
+                <label className="text-black text-medium p-2">Password:</label>
+                <div className="relative">
+                  <input
+                    type={showPassword ? "text" : "password"}
+                    value={password}
+                    onChange={(e) => {
+                      setPassword(e.target.value);
+                      const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).+$/;
+                      if (e.target.value.length >= 8 && passwordRegex.test(e.target.value)) {
+                        setPasswordError("");
+                      }
+                    }}
+                    
+                    className="bg-gray-300 text-black p-2 rounded outline-none focus:ring-2 focus:ring-[#007582] focus:shadow-lg transition rounded outline-none w-full"
+                    required
+                  />
+                  <button
+                    type="button"
+                    onClick={() => setShowPassword(!showPassword)}
+                    className="absolute right-3 top-2 text-gray-600 hover:text-black cursor-pointer"
+                  >
+                    {showPassword ? <FaEyeSlash /> : <FaEye />}
+                  </button>
+                </div>
+                {passwordError && (
+                  <p className="text-red-500 text-sm max-w-sm">{passwordError}</p>
+                )}
+              </div>
+
+              {/*Tombol Register */}
+              <button
+                type="submit"
+                 className="bg-[#007582] hover:bg-[#006060] active:scale-95 text-white font-semibold py-2 rounded transition cursor-pointer w-full"
+              >
+                Register
+              </button>
+
+              <p className="text-sm text-center">
+                Sudah punya akun?{" "}
+                <a href="/login" className="text-gray-500 hover:underline">
+                  Login
+                </a>
+              </p>
+
+            </form>
           </div>
-          {passwordError && (
-            <p className="text-red-500 text-sm">{passwordError}</p>
-          )}
         </div>
 
-        <button
-          type="submit"
-          className="bg-[#007582] text-white font-semibold py-2 rounded transition cursor-pointer"
-        >
-          Register
-        </button>
-      </form>
+        {/*Footer */}
+        <div className="absolute bottom-0 w-full flex justify-between items-center px-8 pb-6 text-xs text-gray-400">
+          <p>Copyright © 2025 ReuseID. All rights reserved.</p>
+          <p>Privacy Policy</p>
+        </div>
+      </div>
 
-      {/* Gambar ilustrasi */}
-      <div className="ml-10">
+      {/* Container kanan: Gambar ilustrasi */}
+      <div className="hidden md:flex flex-1 relative h-screen">
         <Image
           src="/reuse_id.jpeg"
           alt="ReuseID"
-          width={400}
-          height={300}
+          fill
+          quality={100}
+          sizes="100vh"
           className="rounded shadow-md"
         />
       </div>
@@ -156,6 +209,14 @@ export default function RegisterPage() {
         <SuccessPopup
           message={successMessage}
           onClose={() => setSuccessMessage("")}
+        />
+      )}
+
+      {/* popup user sudah terdaftar*/}
+      {errorMessage && (
+        <ErrorPopup
+          message={errorMessage}
+          onClose={() => setErrorMessage("")}
         />
       )}
     </div>
--- components/ErrorPopup.tsx
+++ components/ErrorPopup.tsx
@@ -2,42 +2,44 @@
 import Image from "next/image";
 import { useEffect, useState } from "react";
 
-type SuccessPopupProps = {
+type ErrorPopupProps = {
   message: string;
   onClose: () => void;
 };
 
-export default function SuccessPopup({ message, onClose }: SuccessPopupProps) {
+export default function ErrorPopup({ message, onClose }: ErrorPopupProps) {
   const [visible, setVisible] = useState(true);
+
   // Tutup popup kalau user klik di luar kotak
   useEffect(() => {
     const handleClickOutside = (e: MouseEvent) => {
       const target = e.target as HTMLElement;
-      if (target.closest("#popup-box") === null) {
+      if (target.closest("#error-popup-box") === null) {
         setVisible(false);
-        setTimeout(onClose, 300);
+        setTimeout(onClose, 300); // delay sesuai durasi fadeOut
       }
     };
     document.addEventListener("click", handleClickOutside);
     return () => document.removeEventListener("click", handleClickOutside);
   }, [onClose]);
-
   return (
-    <div className={`fixed inset-0 flex items-center justify-center bg-black/10 z-50
-        ${visible ? "animate-fadeIn" : "animate-fadeOut"}`}>
-
+    <div
+      className={`fixed inset-0 flex items-center justify-center bg-black/10 z-50
+        ${visible ? "animate-fadeIn" : "animate-fadeOut"}`}
+      onClick={onClose}
+    >
       <div
-        id="popup-box"
-        className="bg-white p-6 rounded-lg shadow-xl text-center max-w-sm w-full transform animate-slideDown"
+        onClick={(e) => e.stopPropagation()}
+        className="bg-white p-6 rounded-lg shadow-xl text-center max-w-sm w-full"
       >
         <Image
-          src="/check.png"
-          alt="Success"
+          src="/silang.png"
+          alt="Error"
           width={80}
           height={80}
           className="mx-auto mb-4"
         />
-        <h2 className="text-xl font-bold text-green-600 mb-2">{message}</h2>
+        <h2 className="text-xl font-bold text-red-600 mb-2">{message}</h2>
       </div>
     </div>
   );
