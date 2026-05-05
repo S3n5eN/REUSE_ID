8183b33a4109b3913d1396c0d74b45609bf60812
Glen <glendavid344@gmail.com>
2026-05-05 15:00:43 +0700

    add API profile

---
+++ app/api/Pengguna/profile/route.ts
@@ -0,0 +1,31 @@
+import { prisma } from "@/lib/prisma";
+import { protect } from "@/lib/protect";
+import { NextRequest, NextResponse } from "next/server";
+
+async function getProfile(req: NextRequest, decoded: { id: string }) {
+  try {
+    const userId = Number(decoded.id);
+
+    const user = await prisma.user.findFirst({
+      where: { id : userId},
+      include: {profile: true}
+    });
+
+    return NextResponse.json({
+      message: "Berhasil mengambil data profile",
+      data: user
+    });
+
+  } catch (error: any) {
+    console.error("ERROR GET PROFILE:", error);
+
+    return NextResponse.json(
+      { message: "Gagal mengambil profile" },
+      { status: 500 }
+    );
+  }
+}
+
+export async function GET(req: NextRequest) {
+  return (await protect(getProfile, ["user"]))(req);
+}
