3725a294778ef1a2c92cd86a5962d28fe202ea5c
Xuerns <dmmaulana06@gmail.com>
2026-05-18 13:20:19 +0700

    refactor: merge migrasi postgresSQL

--- app/api/Barang/getImage/[id]/route.ts
+++ app/api/Barang/getImage/[id]/route.ts
@@ -1,34 +1,36 @@
 import { NextRequest, NextResponse } from "next/server";
 import { prisma } from "@/lib/prisma";
-import { protect } from "@/lib/protect";
+import { Buffer } from "buffer";
 
-async function getImage(
-    req: NextRequest, 
-    { params }: { params: Promise<{ id: string }> } 
+export async function GET(
+  req: NextRequest,
+  { params }: { params: Promise<{ id: string }> }
 ) {
-    try {
-        const resolvedParams = await params;
-        const imageId = resolvedParams.id;
+  try {
+    const resolvedParams = await params;
+    const imageId = Number(resolvedParams.id);
 
-        const imageData = await prisma.item.findUnique({
-            where: { id: Number(imageId) },
-        });
+    const imageData = await prisma.item.findUnique({
+      where: { id: imageId },
+    });
 
-        if (!imageData || !imageData.imageData) {
-            return new NextResponse("Image not found", { status: 404 });
-        }
-
-        return new NextResponse(imageData.imageData, {
-            headers: {
-                "Content-Type": imageData.imageType,
-                "Cache-Control": "public, max-age=31536000, immutable",
-            },
-        });
-    } catch {
-        return new NextResponse("Gagal mengambil gambar", { status: 500 });
+    if (!imageData || !imageData.imageData) {
+      return new NextResponse("Image not found", {
+        status: 404,
+      });
     }
-}
 
-export async function GET(req: NextRequest)  {
-    return (await protect(getImage, ["admin", "user"]))(req);
+    return new NextResponse(Buffer.from(imageData.imageData), {
+      headers: {
+        "Content-Type": imageData.imageType || "image/webp",
+        "Cache-Control": "public, max-age=31536000, immutable",
+      },
+    });
+  } catch (error) {
+    console.error(error);
+
+    return new NextResponse("Gagal mengambil gambar", {
+      status: 500,
+    });
+  }
 }
